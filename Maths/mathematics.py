705e7351b52407d8fe56077197daf7bf9a65d795
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-10-15 17:17:15 +0300

    adding mathmatics add, subtract, mutiply and error handling

---
+++ Maths/mathematics.py
@@ -0,0 +1,21 @@
+def summation(a,b):
+    """this function sum 2 numbers
+    Returns: the summation
+    """
+    result = a+b
+    return result
+
+def subraction(a,b):
+    """this function subract 2 numbers
+    Returns: the subraction
+    """
+    result = a-b
+    return result
+
+def multipliacation(a,b):
+    """this function mulitply 2 numbers
+    Returns: the mulitplication
+    """
+    result = a*b
+    return result
+
--- server.py
+++ server.py
@@ -1,29 +1,46 @@
 from flask import Flask, render_template, request
+from Maths.mathematics import summation,subraction,multipliacation
 # Import the Maths package here
 
 app = Flask("Mathematics Problem Solver")
 
 @app.route("/sum")
 def sum_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
+    try:
+        num1 = float(request.args.get('num1'))
+        num2 = float(request.args.get('num2'))
+        result = summation(num1, num2)
+        return str(result)
+    except(ValueError, TypeError):
+        return "Invalid input. Please provide numeric values for 'num1' and 'num2.", 400
     # Write your code here
 
 @app.route("/sub")
 def sub_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
+    try:
+        num1 = float(request.args.get('num1'))
+        num2 = float(request.args.get('num2'))
+        result = subraction(num1, num2)
+        return str(result)
+    except(ValueError, TypeError):
+        return "Invalid input. Please provide numeric values for 'num1' and 'num2.", 400
     # Write your code here
 
 @app.route("/mul")
 def mul_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
+    try:
+        num1 = float(request.args.get('num1'))
+        num2 = float(request.args.get('num2'))
+        result = multipliacation(num1, num2)
+        return str(result)
+    except(TypeError, ValueError):
+        return "Invalid input. Please provide numeric values for 'num1' and 'num2.", 400
     # Write your code here  
 
 @app.route("/")
 def render_index_page():
     # Write your code here
+    return render_template('index.html')
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
